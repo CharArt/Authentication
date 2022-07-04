9dcb24b2aac9c9e292cb3c39e0ef5e6da907217b
Artem Charykov <67795388+CharArt@users.noreply.github.com>
2022-07-05 00:18:24 +0300

    Version 0.0.0.7

---
+++ src/main/java/com/converter/converter/auth/tools/JwtAuthenticationException.java
@@ -0,0 +1,23 @@
+package com.converter.converter.auth.tools;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+
+    private HttpStatus status;
+
+    public JwtAuthenticationException(String msg, HttpStatus status) {
+        super(msg);
+        this.status = status;
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
